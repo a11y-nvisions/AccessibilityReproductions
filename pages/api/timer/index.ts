85990feb10f2fba9597052ed04f76bdb81eeebba
= <=>
2022-11-02 15:13:29 +0900

    api test 2

--- pages/api/timer/index.ts
+++ pages/api/timer/index.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function timer ( rq:NextApiRequest, rs:NextApiResponse ) {
-  const date = new Date()
-  rs.status(200).json({
-    h:date.getHours(),
-    m:date.getMinutes(),
-    s:date.getSeconds(),
-    ms:date.getMilliseconds()
-  })
+  const date = new Date();
+  if (rq.method == "GET") {
+    rs.status(200).json({
+      h:date.getHours(),
+      m:date.getMinutes(),
+      s:date.getSeconds(),
+      ms:date.getMilliseconds()
+    })
+  }
 }
