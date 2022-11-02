dccf5298388ed3aee713c46bf4cf3f00cf2baf25
= <=>
2022-11-02 14:52:39 +0900

    api test

--- pages/api/timer/index.ts
+++ pages/api/timer/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function hanlder( rq:NextApiRequest, rs:NextApiResponse ) {
+export default function timer ( rq:NextApiRequest, rs:NextApiResponse ) {
   const date = new Date()
   rs.status(200).json({
     h:date.getHours(),
